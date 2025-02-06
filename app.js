e0b51f205bf06abdb6c5d033ba3bda821079ca15
GUSTAVO ZUMBA DE AMORIM <24124573@cfp127aluno.local>
2025-02-06 11:57:27 -0300

    Card ok

--- app.js
+++ app.js
@@ -6,6 +6,14 @@ const disciplinas = [
     {nome:'PWBE', icon:'web.png' ,cor:'blue'},
     {nome:'PWFE',icon: 'youtube.png',cor:"pink"}
 ]
+const produtos = [
+    {nome:'Camiseta Gorrilaz', camiseta:'gorillaz.png',  descrição: 'Banda Gorillaz'  , preço:'R$:100,00'},
+    {nome:'Camiseta Nautiruts', camiseta:'nautiruts.png', descrição: 'Camiseta banda de reagge Nautiruts' , preço:'R$:150,00'},
+    {nome:'Camiseta Kiss', camiseta:'kiss.png', preço:'R$:110,00'},
+    {nome:'Camiseta Zeca Pagodinho', camiseta:'pagodinho.png', preço:'R$:300,00'},
+    {nome:'Camiseta System of Down', camiseta:'system.png', preço:'R$:190,00'},
+   
+]
 
 function criarMenu(disciplina){
     const novoItem = document.createElement('li')
@@ -22,5 +30,48 @@ function criarMenu(disciplina){
 
     lista.appendChild(novoItem)
 }
-
 disciplinas.forEach(criarMenu)
+
+
+const cardsContainer = document.getElementById("cards-container");
+
+produtos.forEach(produto => {
+  const card = document.createElement("div")
+  card.classList.add("card")
+
+  const imagem = document.createElement("img")
+  imagem.src = `./img/${produto.camiseta}`
+  imagem.alt = produto.nome
+  card.appendChild(imagem)
+
+  const nome = document.createElement("h3")
+  nome.textContent = produto.nome
+  card.appendChild(nome)
+
+  const descrição = document.createElement("p")
+  descrição.textContent = produto.descrição
+  card.appendChild(descrição)
+
+  const preco = document.createElement("p")
+  preco.textContent = produto.preço
+  card.appendChild(preco)
+
+  const tamanhos = document.createElement("div")
+  tamanhos.classList.add("tamanhos")
+
+  const tamanhosDisponiveis = ["P", "M", "G"]
+  tamanhosDisponiveis.forEach(tamanho => {
+    const botaoTamanho = document.createElement("button")
+    botaoTamanho.textContent = tamanho
+    tamanhos.appendChild(botaoTamanho)
+  });
+
+  card.appendChild(tamanhos)
+
+  const botaoComprar = document.createElement("button")
+  botaoComprar.classList.add("comprar")
+  botaoComprar.textContent = "Add to Cart"
+  card.appendChild(botaoComprar)
+
+  cardsContainer.appendChild(card)
+});
