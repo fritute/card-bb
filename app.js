15b7dba3da57a4cb0a7057836dc5014cf16f04dc
GUSTAVO ZUMBA DE AMORIM <24124573@cfp127aluno.local>
2025-02-06 10:45:04 -0300

    js ok

--- app.js
+++ app.js
@@ -1,1 +1,26 @@
 'use strict'
+
+const disciplinas = [
+    {nome :'PPDM', icon: 'insta.png', cor:'yellow'},
+    {nome :'PRO' ,icon: 'tiktok.png', cor:'tomato'},
+    {nome:'PWBE', icon:'web.png' ,cor:'blue'},
+    {nome:'PWFE',icon: 'youtube.png',cor:"pink"}
+]
+
+function criarMenu(disciplina){
+    const novoItem = document.createElement('li')
+    const novaImagem = document.createElement('img')
+    const novoSpan = document.createElement('span')
+    const lista = document.getElementById('menu')
+
+    novaImagem.src =`./img/${disciplina.icon}`
+    novoSpan.textContent = disciplina.nome
+
+    novoItem.appendChild(novaImagem)
+    novoItem.appendChild(novoSpan)
+    novoItem.style = `--cor-hover:${disciplina.cor}`
+
+    lista.appendChild(novoItem)
+}
+
+disciplinas.forEach(criarMenu)
